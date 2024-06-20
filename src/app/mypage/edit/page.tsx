6a24002721529b01a105088106c02d41fdc8e1f0
injulme <injulme0309@naver.com>
2024-06-20 22:39:39 +0900

    feat: 마이페이지 프로필 수정 ui 구현

--- src/app/mypage/edit/page.tsx
+++ src/app/mypage/edit/page.tsx
@@ -1,11 +1,38 @@
 'use client';
 
+import { useForm } from 'react-hook-form';
+
+import { z } from 'zod';
+
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
 import { Button } from '@/components/ui/button';
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+import { Textarea } from '@/components/ui/textarea';
 
 export default function MyPageEdit() {
+  const form = useForm();
+
+  const formSchema = z.object({
+    username: z.string().min(2, {
+      message: 'Username must be at least 2 characters.',
+    }),
+  });
+
+  const onSubmit = () => {
+    console.log('submit');
+  };
+
   return (
-    <section>
+    <section className="mx-[120px] mt-[80px]">
       <div className="flex gap-16">
         <div className="flex flex-col items-center gap-6">
           <Avatar className="h-[200px] w-[200px] shadow-xl">
@@ -14,11 +41,124 @@ export default function MyPageEdit() {
           </Avatar>
           <Button>프로필 변경</Button>
         </div>
-        <div>
+        <div className="w-full">
           <div>프로필 정보 / 계좌 정보 / 회원 탈퇴</div>
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+              <FormField
+                control={form.control}
+                name="username"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>닉네임</FormLabel>
+                    <FormControl>
+                      <Input placeholder="입력하세요" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="username"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>이메일</FormLabel>
+                    <FormControl>
+                      <Input placeholder="입력하세요" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="username"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>전화번호</FormLabel>
+                    <FormControl>
+                      <Input placeholder="입력하세요" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="username"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>관심 카테고리</FormLabel>
+                    <FormControl>
+                      <Input placeholder="입력하세요" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="username"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>자기소개</FormLabel>
+                    <FormControl>
+                      <Textarea
+                        placeholder="Type your message here."
+                        {...field}
+                        className="resize-none"
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="username"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>개인 블로그</FormLabel>
+                    <FormControl>
+                      <Input placeholder="입력하세요" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="username"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>인스타그램</FormLabel>
+                    <FormControl>
+                      <Input placeholder="입력하세요" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="username"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>유튜브</FormLabel>
+                    <FormControl>
+                      <Input placeholder="입력하세요" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
 
-          <div>nicname</div>
-          <Button>수정하기</Button>
+              <div className="text-center">
+                <Button type="submit">수정하기</Button>
+              </div>
+            </form>
+          </Form>
         </div>
       </div>
     </section>
