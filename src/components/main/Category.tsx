36d4dc3be6766c8e89a4ad7d977ddd824ca5a96c
injulme <injulme0309@naver.com>
2024-07-04 21:05:12 +0900

    feat: 카테고리 아이콘 변경

--- src/components/main/Category.tsx
+++ src/components/main/Category.tsx
@@ -1,3 +1,5 @@
+import { PiBookBookmarkLight } from 'react-icons/pi';
+
 import { codeToOptions } from '@/lib/utils';
 
 import { Button } from '@/components/ui/button';
@@ -11,76 +13,12 @@ export default function Category() {
     <div className="pb-12">
       <div className="space-y-4 py-4">
         <div className="px-3 py-2">
-          <h2 className="mb-2 px-4 text-lg font-semibold tracking-tight">일반</h2>
+          <h2 className="mb-2 px-4 text-lg font-semibold tracking-tight">카테고리</h2>
           <div className="space-y-1">
             {categories.map((category) => {
               return (
                 <Button variant="ghost" className="w-full justify-start" key={category.value}>
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    stroke="currentColor"
-                    strokeWidth="2"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    className="mr-2 h-4 w-4"
-                  >
-                    <circle cx="12" cy="12" r="10" />
-                    <polygon points="10 8 16 12 10 16 10 8" />
-                  </svg>
-                  {category.label}
-                </Button>
-              );
-            })}
-          </div>
-        </div>
-        <div className="px-3 py-2">
-          <h2 className="mb-2 px-4 text-lg font-semibold tracking-tight">자기계발</h2>
-          <div className="space-y-1">
-            {categories.map((category) => {
-              return (
-                <Button variant="ghost" className="w-full justify-start" key={category.value}>
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    stroke="currentColor"
-                    strokeWidth="2"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    className="mr-2 h-4 w-4"
-                  >
-                    <circle cx="8" cy="18" r="4" />
-                    <path d="M12 18V2l7 4" />
-                  </svg>
-                  {category.label}
-                </Button>
-              );
-            })}
-          </div>
-        </div>
-        <div className="px-3 py-2">
-          <h2 className="mb-2 px-4 text-lg font-semibold tracking-tight">시/에세이</h2>
-          <div className="space-y-1">
-            {categories.map((category) => {
-              return (
-                <Button variant="ghost" className="w-full justify-start" key={category.value}>
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    stroke="currentColor"
-                    strokeWidth="2"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    className="mr-2 h-4 w-4"
-                  >
-                    <path d="m16 6 4 14" />
-                    <path d="M12 6v14" />
-                    <path d="M8 8v12" />
-                    <path d="M4 4v16" />
-                  </svg>
+                  <PiBookBookmarkLight size={24} className="mr-2 h-4 w-4" />
                   {category.label}
                 </Button>
               );
