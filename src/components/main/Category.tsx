b9b9928880cd3f7acc3d3d688e97df35156c5d61
injulme <injulme0309@naver.com>
2024-07-22 22:57:02 +0900

    feat: 카테고리 스크롤 고정

--- src/components/main/Category.tsx
+++ src/components/main/Category.tsx
@@ -10,20 +10,18 @@ const categories = codeToOptions(CategoryTypeCode);
 
 export default function Category() {
   return (
-    <div className="pb-12">
-      <div className="space-y-4 py-4">
-        <div className="px-3 py-2">
-          <h2 className="mb-2 px-4 text-lg font-semibold tracking-tight">카테고리</h2>
-          <div className="space-y-1">
-            {categories.map((category) => {
-              return (
-                <Button variant="ghost" className="w-full justify-start" key={category.value}>
-                  <PiBookBookmarkLight size={24} className="mr-2 h-4 w-4" />
-                  {category.label}
-                </Button>
-              );
-            })}
-          </div>
+    <div>
+      <div className="sticky top-24 flex flex-col px-3 py-2">
+        <h2 className="mb-2 px-4 text-lg font-semibold tracking-tight">카테고리</h2>
+        <div className="space-y-1">
+          {categories.map((category) => {
+            return (
+              <Button variant="ghost" className="w-full justify-start" key={category.value}>
+                <PiBookBookmarkLight size={24} className="mr-2 h-4 w-4" />
+                {category.label}
+              </Button>
+            );
+          })}
         </div>
       </div>
     </div>
