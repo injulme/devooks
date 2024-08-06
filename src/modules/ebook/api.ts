be9a3d9d4cbe04a0167b4df9ccbcbd35a5d5bcd6
injulme <injulme0309@naver.com>
2024-08-06 21:19:37 +0900

    fix: ebook api return 오류 수정

--- src/modules/ebook/api.ts
+++ src/modules/ebook/api.ts
@@ -1,7 +1,7 @@
 import api from '@/lib/api';
 
 export async function getEbooks(): Promise<void> {
-  await api({
+  return await api({
     method: 'get',
     url: `/api/v1/ebooks?page=1&count=1`,
   });
