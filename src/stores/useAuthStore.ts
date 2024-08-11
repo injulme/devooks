4bd3b74a3352de396ad23a1f6769f72db08047de
injulme <injulme0309@naver.com>
2024-08-11 16:40:05 +0900

    feat: useAuthStore 구현

---
+++ src/stores/useAuthStore.ts
@@ -0,0 +1,34 @@
+import { create } from 'zustand';
+import { createJSONStorage, persist } from 'zustand/middleware';
+
+import cookieStorage from '@/lib/cookie-storage';
+
+import { LoginResponse } from '@/modules/login/type';
+
+type State = Pick<LoginResponse['tokenGroup'], 'accessToken' | 'refreshToken'>;
+
+type Actions = {
+  updateAuthMetadata: (data: State) => void;
+  logout: () => void;
+};
+
+const initialState: State = {
+  accessToken: null,
+  refreshToken: null,
+};
+export const useAuthStore = create(
+  persist<State & Actions>(
+    (set, get) => ({
+      ...initialState,
+      updateAuthMetadata: (data: State) => set({ ...data }),
+      logout: () => {
+        set(initialState);
+        window.location.href = '/login';
+      },
+    }),
+    {
+      name: 'auth-metadata',
+      storage: createJSONStorage(() => cookieStorage),
+    },
+  ),
+);
