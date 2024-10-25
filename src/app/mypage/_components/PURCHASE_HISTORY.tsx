9305cd5b13e19da44f88763d25891961b54ad149
injulme <injulme0309@naver.com>
2024-10-25 23:22:20 +0900

    feat: mypage 책 내역 리스트 ui 수정
    #DVK-32

--- src/app/mypage/_components/PURCHASE_HISTORY.tsx
+++ src/app/mypage/_components/PURCHASE_HISTORY.tsx
@@ -1,6 +1,10 @@
+import Image from 'next/image';
+
+import cover1 from '@/assets/images/cover1.png';
+
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
 import { Button } from '@/components/ui/button';
-import { Card, CardContent, CardDescription } from '@/components/ui/card';
+import { Card, CardContent } from '@/components/ui/card';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 
@@ -23,34 +27,45 @@ export default function PurchaseHistory() {
           return (
             <Card className="shadow-lg" key={d}>
               <CardContent className="mt-4">
-                <p className="text-md mb-2 text-right text-muted-foreground">
+                <p className="mb-2 text-right text-xs text-muted-foreground">
                   주문일시 <span className="text-slate-900">2023.01.07 18:03</span>
                 </p>
-                <div className="flex w-full gap-5">
-                  <div className="w-[240px] rounded-sm bg-slate-700 shadow">box {d}</div>
-                  <div className="flex w-full flex-col">
-                    <p className="text-sm text-zinc-500">#category #category #category</p>
-                    <h2 className="scroll-m-20 pb-2 text-2xl font-semibold tracking-tight first:mt-0">
-                      The People of the Kingdom
-                    </h2>
-                    <div className="flex items-center gap-2">
-                      <Avatar className="h-[28px] w-[28px] shadow-xl">
-                        <AvatarImage src="https://github.com/shadcn.png" />
-                        <AvatarFallback>CN</AvatarFallback>
-                      </Avatar>
-                      <h3 className="text-2xl font-semibold tracking-tight">upn2n</h3>
+                <div className="flex w-full gap-6">
+                  <Image
+                    src={cover1}
+                    alt="썸네일"
+                    className="max-w-[120px] rounded bg-no-repeat object-cover"
+                  />
+
+                  <div className="flex w-full flex-col justify-between">
+                    <div>
+                      <p className="text-xs text-zinc-500">#category #category #category</p>
+                      <h2 className="scroll-m-20 pb-2 text-xl font-semibold tracking-tight first:mt-0">
+                        The People of the Kingdom
+                      </h2>
+                      <div className="flex items-center gap-2">
+                        <Avatar className="h-[28px] w-[28px] shadow-xl">
+                          <AvatarImage src="https://github.com/shadcn.png" />
+                          <AvatarFallback>CN</AvatarFallback>
+                        </Avatar>
+                        <h3 className="text-md font-semibold tracking-tight">upn2n</h3>
+                      </div>
                     </div>
-                    <div className="flex items-end justify-between">
+                    <div className="flex items-end justify-between sm:flex-col sm:gap-1 md:flex-row">
                       <h4 className="text-md scroll-m-20 font-semibold tracking-tight">
                         3,980,000원{' '}
                         <span className="text-xs font-medium text-muted-foreground">
                           (VAT 포함)
                         </span>
                       </h4>
-                      <div className="space-x-2">
-                        <Button variant="outline">리뷰작성</Button>
-                        <Button>미리보기</Button>
-                        <Button>다운로드</Button>
+                      <div className="space-x-1">
+                        <Button variant="secondary" size="sm">
+                          리뷰작성
+                        </Button>
+                        <Button size="sm" variant="outline">
+                          미리보기
+                        </Button>
+                        <Button size="sm">다운로드</Button>
                       </div>
                     </div>
                   </div>
