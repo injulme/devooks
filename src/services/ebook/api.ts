13e8261a7f32b288d223fde3dc4debe921750653
injulme <injulme0309@naver.com>
2024-11-12 21:32:20 +0900

    chore: get ebook api return type 변경
    #DVK-22

--- src/services/ebook/api.ts
+++ src/services/ebook/api.ts
@@ -3,8 +3,9 @@ import { EbookListGetResponse } from './type';
 import api from '@/lib/api';
 
 export async function getEbooks(): Promise<EbookListGetResponse> {
-  return await api({
+  const { data } = await api({
     method: 'get',
     url: `/api/v1/ebooks?page=1&count=1`,
   });
+  return data;
 }
