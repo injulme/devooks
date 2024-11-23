33f5792309d44c6f285d30dfaa360ad9865515e6
injulme <injulme0309@naver.com>
2024-11-24 02:12:14 +0900

    refactor: 카테고리 정의된 코드 삭제
    #DVK-27

--- src/constant/common.ts
+++ src/constant/common.ts
@@ -21,56 +21,6 @@ export const TabTypeCode: CodeType<TabType> = {
   },
 };
 
-export type CategoryType =
-  | 'PROGRAMMING'
-  | 'GAME'
-  | 'BUSINESS'
-  | 'AI'
-  | 'HARDWARE'
-  | 'DESIGN'
-  | 'FINANCE'
-  | 'LIBERAL_ART'
-  | 'START_UP';
-
-export const CategoryTypeCode: CodeType<CategoryType> = {
-  PROGRAMMING: {
-    label: 'IT / 프로그래밍',
-    value: 'PROGRAMMING',
-  },
-  GAME: {
-    label: '게임',
-    value: 'GAME',
-  },
-  BUSINESS: {
-    label: '비즈니스',
-    value: 'BUSINESS',
-  },
-  HARDWARE: {
-    label: '하드웨어',
-    value: 'HARDWARE',
-  },
-  AI: {
-    label: '인공지능',
-    value: 'AI',
-  },
-  DESIGN: {
-    label: '디자인',
-    value: 'DESIGN',
-  },
-  FINANCE: {
-    label: '금융 / 재테크',
-    value: 'FINANCE',
-  },
-  LIBERAL_ART: {
-    label: '교양',
-    value: 'LIBERAL_ART',
-  },
-  START_UP: {
-    label: '창업',
-    value: 'START_UP',
-  },
-};
-
 export type BookDetailTabType = 'INTRODUCTION' | 'TABLE_OF_CONTENTS' | 'CLAIM' | 'REVIEW';
 export const BookDetailTabTypeCode: CodeType<BookDetailTabType> = {
   INTRODUCTION: {
