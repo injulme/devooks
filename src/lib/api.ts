c6fd4f1eb2f3389e3ec3601d2ab5860caccaaee1
injulme <injulme0309@naver.com>
2024-11-23 17:08:38 +0900

    feat: api module 토큰 관리 로직 수정
    - refresh 로직 개선
    
    #DVK-27

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,6 +1,8 @@
-import cookieStorage from './cookie-storage';
-
 import axios from 'axios';
+
+import { useTokenStore } from '@/stores/auth-store';
+
+const tokenStore = useTokenStore.getState();
 
 const api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_BASE_URL,
@@ -11,7 +13,7 @@ const api = axios.create({
 
 api.interceptors.request.use(
   (request) => {
-    const accessToken = cookieStorage.getItem('accessToken');
+    const accessToken = tokenStore.accessToken;
     if (accessToken) {
       request.headers['Authorization'] = `Bearer ${accessToken}`;
     }
@@ -29,23 +31,23 @@ api.interceptors.response.use(
     if (error.response.status === 401 && !originalRequest._retry) {
       originalRequest._retry = true;
       try {
-        const refreshToken = cookieStorage.getItem('refreshToken');
+        const refreshToken = tokenStore.refreshToken;
 
         const response = await axios.post('/api/v1/auth/reissue', {
           refreshToken,
         });
         const { accessToken, refreshToken: newRefreshToken } = response.data;
 
-        cookieStorage.setItem('accessToken', accessToken);
-        cookieStorage.setItem('refreshToken', newRefreshToken);
+        tokenStore.updateToken({
+          accessToken,
+          refreshToken: newRefreshToken,
+        });
 
         api.defaults.headers.common['Authorization'] = `Bearer ${accessToken}`;
 
         return api(originalRequest);
       } catch (refreshError) {
-        cookieStorage.removeItem('accessToken');
-        cookieStorage.removeItem('refreshToken');
-        window.location.href = '/login';
+        tokenStore.logout();
 
         return Promise.reject(refreshError);
       }
