72880ecb4a4f1a9ab28432bc2c9a0f63b154917b
injulme <injulme0309@naver.com>
2024-11-25 21:57:30 +0900

    chore: wishlist layout 수정
    #DVK-33

--- src/app/mypage/_components/WISHLIST.tsx
+++ src/app/mypage/_components/WISHLIST.tsx
@@ -1,17 +1,12 @@
-import Link from 'next/link';
-
 import { useGetWishlist } from '@/services/wishlist/hooks/useGetWishlist';
 
 import BookContainer from '@/components/ebook/book-container';
 
-const dummy = Array(15)
-  .fill({})
-  .map((_, i) => i);
 export default function Wishlist() {
   const { data } = useGetWishlist();
 
   return (
-    <div className="grid gap-x-8 gap-y-12 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+    <div className="grid gap-x-8 gap-y-12 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
       {data?.data.map((ebook) => {
         return <BookContainer bookData={ebook} key={ebook.id} />;
       })}
